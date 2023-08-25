e6448d8fda1588c7f72e9b2057859aef327a5353
yellowberard <goelmanu002@gmail.com>
2023-08-25 14:07:18 +0530

    first ques added

---
+++ striver_a-z/Arrays/Medium/stock_buy_sell.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+//
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int max = 0;
+        int min = INT_MAX;
+        // for(int i=0;i<prices.size();i++){
+        //     for(int j=i+1;j<prices.size();j++){
+        //         if((prices[j]-prices[i])>max){
+        //             max=prices[j]-prices[i];
+        //         }
+        //     }
+        // }
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices[i] < min)
+            {
+                min = prices[i];
+            }
+            if ((prices[i] - min) > max)
+            {
+                max = prices[i] - min;
+            }
+        }
+        return max;
+    }
+};
