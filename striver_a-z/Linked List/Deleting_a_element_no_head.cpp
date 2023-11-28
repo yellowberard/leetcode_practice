b1be2f2c738058a4066f231f0e43a781a46df49d
yellowberard <goelmanu002@gmail.com>
2023-11-28 09:11:23 +0530

    added a ques

---
+++ striver_a-z/Linked List/Deleting_a_element_no_head.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+
+ Definition for singly-linked list.
+  struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+
+        node->val = node->next->val;   // copying the next node value
+        node->next = node->next->next; // deleting the next node
+    }
+};
